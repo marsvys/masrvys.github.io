cd85e526fadad85e4d8ad7d5146d513007cc5547
marsvys <156508333+marsvys@users.noreply.github.com>
2024-01-13 16:59:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you on the 18th!";
+  question.innerHTML = "so sud yodddd!!! i love you muan gan krubsss eieiei";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
    // hides the "No" button after clicking "Yes"
